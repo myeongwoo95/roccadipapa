a0e6e2aa4228e8606970805f0921c5ea77b7b0aa
walle <myeongwoo95@naver.com>
2023-01-06 20:07:08 +0900

    :>

---
+++ src/main/java/com/myeongwoo/roccadipapa/RoccadipapaApplication.java
@@ -0,0 +1,13 @@
+package com.myeongwoo.roccadipapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoccadipapaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoccadipapaApplication.class, args);
+    }
+
+}
